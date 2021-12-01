cf2c1aa5cab8dcdb40076b678b5c068f781a10d1
KimHyeonsang <62978367+KimHyeonsang@users.noreply.github.com>
2021-12-01 11:49:41 +0900

    update

---
+++ UI/Assets/AudioPlayer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPlayer : MonoBehaviour
+{
+    private AudioSource SoundPlayer;
+    private void Awake()
+    {
+    //    SoundPlayer = SoundManager.GetInstance.GetAudioClip(0);
+    }
+    void Start()
+    {
+        SoundPlayer = GetComponent<AudioSource>();
+        SoundPlayer.clip = SoundManager.GetInstance.GetAudioClip(0);
+        PlaySound();
+    }
+
+    public void PlaySound(bool _Loop = false)
+    {
+        SoundPlayer.volume = 1.0f;
+        //즉시실행
+        SoundPlayer.time = 0;
+        SoundPlayer.loop = _Loop;
+
+        SoundPlayer.Play();
+    }
+
+    public void StopSound()
+    {
+        SoundPlayer.Stop();
+    }
+}
